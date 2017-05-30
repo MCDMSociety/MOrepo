ef53bb69ffb890631e7d1fdcf00c5ac29bf33abd
relund <junk@relund.dk>
2017-05-30 18:25:29 +0200

    Package doc updated

--- misc/R/MOrepoTools/R/check.R
+++ misc/R/MOrepoTools/R/check.R
@@ -1,13 +1,22 @@
-# files must be UTF-8 (ask for it)
-#
 
-
+#' Validate meta file based on schema.
+#'
+#' @param meta  Name of meta file.
+#'
+#' @return Warnings and errors (if any).
+#' @author Lars Relund \email{lars@@relund.dk}
 checkMeta<-function(meta) {
    schema<-system.file("metaSchema.json", package = "MOrepoTools")
    jsonvalidate::json_validate(meta,schema, verbose = TRUE, error = TRUE)
 }
 
 
+#' Function of asking questions.
+#'
+#' @param ...  Question string.
+#'
+#' @return True if yes and false otherwise.
+#' @author Lars Relund \email{lars@@relund.dk}
 yesno<-function (...)
 {
    yeses <- c("Yes", "Yup", "Yeah")
@@ -19,6 +28,12 @@ yesno<-function (...)
 }
 
 
+#' Check if a contribution convey with the guidelines.
+#'
+#' You must run this function in the root of your contribution.
+#'
+#' @author Lars Relund \email{lars@@relund.dk}
+#' @export
 checkContribution<-function() {
    # message("Your working directory is ", getwd())
    # if (!yesno("Is that the location of your contribution?")) {
@@ -39,42 +54,50 @@ checkContribution<-function() {
    }
    checkMeta("meta.json")
    meta<-jsonlite::fromJSON("meta.json")
-   if (meta$folder != basename(getwd())) {
+   if (meta$folder != sub("MOrepo-", "", basename(getwd()))) {
       message("\n   Error: The folder entry in meta.json is not equal the folder name of the contribution!")
       return(invisible(FALSE))
    }
    message(" ok.")
 
-   message("Checking instance subfolders ...", appendLF = FALSE)
-   # Subfolders for each file format
-   if (!all(meta$instanceGroups$format[[1]] %in% list.dirs(full.names = FALSE, recursive = FALSE))) {
-      message("\n   Error: Your contribution must have a subfolder for each instance file format (",
-              paste0(meta$instanceGroups$format[[1]], collapse = ", "), ").")
-      return(invisible(FALSE))
-   }
-   # Subfolders for each instance group
-   for (f in meta$instanceGroups$format[[1]]) {
-      if (!all(list.dirs(f, full.names = FALSE, recursive = FALSE) %in% meta$instanceGroups$subfolder)) {
-         message("\n   Error: The folder ", f, " does not have subfolder(s) ",
-                 paste0(meta$instanceGroups$subfolder, collapse = ", "), "!")
+   if ("instances" %in% list.dirs(full.names = FALSE, recursive = FALSE)) {
+      message("Your contribution contains test instances. ")
+      if (!file.exists("instances/ReadMe.md")) {
+         message("\n   Error: You need to add a file ReadMe.md in the instances folder!")
          return(invisible(FALSE))
       }
-   }
-   # Folder name used as prefix for all instances
-   for (f in meta$instanceGroups$format[[1]]) {
-      if (length(grep(meta$folder, basename(list.files(f, recursive = TRUE)), invert = TRUE))>0) {
-         message("\n   Error: Files in folder ", f, " must all start with prefix ", meta$folder, "!")
+
+      message("Checking instance subfolders ...", appendLF = FALSE)
+      # Subfolders for each file format
+      if (!all(meta$instanceGroups$format[[1]] %in% list.dirs(path = "./instances", full.names = FALSE, recursive = FALSE))) {
+         message("\n   Error: The instances folder must have a subfolder for each instance file format (",
+                 paste0(meta$instanceGroups$format[[1]], collapse = ", "), ") specified in the meta.json file.")
          return(invisible(FALSE))
       }
-   }
-   # Only files with the file format suffix
-   for (f in meta$instanceGroups$format[[1]]) {
-      if (!all(f == tools::file_ext(basename(list.files(f, recursive = TRUE))))) {
-         message("\n   Error: Files in folder ", f, " must end with file suffix ", f, "!")
-         return(invisible(FALSE))
+      # Subfolders for each instance group
+      for (f in meta$instanceGroups$format[[1]]) {
+         if (!all(list.dirs(paste0("./instances/",f), full.names = FALSE, recursive = FALSE) %in% meta$instanceGroups$subfolder)) {
+            message("\n   Error: The folder ", f, " does not have subfolder(s) ",
+                    paste0(meta$instanceGroups$subfolder, collapse = ", "), "!")
+            return(invisible(FALSE))
+         }
       }
+      # Folder name used as prefix for all instances
+      for (f in meta$instanceGroups$format[[1]]) {
+         if (length(grep(meta$folder, basename(list.files(paste0("./instances/",f), recursive = TRUE)), invert = TRUE))>0) {
+            message("\n   Error: Files in folder ", f, " must all start with prefix ", meta$folder, "!")
+            return(invisible(FALSE))
+         }
+      }
+      # Only files with the file format suffix
+      for (f in meta$instanceGroups$format[[1]]) {
+         if (!all(f == tools::file_ext(basename(list.files(paste0("./instances/",f), recursive = TRUE))))) {
+            message("\n   Error: All files in folder ", f, " must end with file suffix ", f, "!")
+            return(invisible(FALSE))
+         }
+      }
+      message(" ok.")
    }
-   message(" ok.")
 
    message("Checking for missing files ...", appendLF = FALSE)
    if (!file.exists("citation.bib")) {
@@ -82,12 +105,12 @@ checkContribution<-function() {
       return(invisible(FALSE))
    }
    if (!file.exists("ReadMe.md")) {
-      message("\n   Error: You need to add a file ReadMe.md!")
+      message("\n   Error: You need to add a file ReadMe.md in the root!")
       return(invisible(FALSE))
    }
    message(" ok.")
 
-   message("Checking for citation.bib ...", appendLF = FALSE)
+   message("Checking citation.bib ...", appendLF = FALSE)
    RefManageR::ReadBib("citation.bib")
    message(" ok.")
 
